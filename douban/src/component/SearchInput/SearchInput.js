cff6302a9434af6439aee6572c1f3cacbcc20bc1
张越 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-02-19 21:31:55 +0800

    search

--- douban/src/component/SearchInput/SearchInput.js
+++ douban/src/component/SearchInput/SearchInput.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
+import style from './SearchInput.css'
+import TextField from 'material-ui/TextField'
 
 class SearchInput extends Component {
 
     /**
      * 搜索框输入
+     * @param {Object} ev event
+     * @param {String} newValue 搜索框内容
      */
-    handleInputChange (ev) {
+    handleInputChange = (ev, newValue) => {
+        this.props.onInputChange(newValue)
+    }
 
+    /**
+     * 清空搜索框
+     */
+    handleInputClose = () => {
+        this.props.onInputClose()
     }
 
     render () {
+        const { value } = this.props
         return (
-            <div>
-                <i class="iconfont icon-sousuo"></i>
-                <input type="text" onChange={this.handleInputChange}/>
-                <i class="iconfont icon-cuowu"></i>
+            <div className={style['input-wrapper']}>
+                <TextField
+                    value={value}
+                    onChange={this.handleInputChange}
+                    hintText="搜索电影"
+                    className={style['input-text']}
+                />
+                {
+                    value !== '' && <i className="iconfont icon-cuowu" onClick={this.handleInputClose}></i>
+                }
             </div>
         )
     }
--- douban/src/page/Search/Search.js
+++ douban/src/page/Search/Search.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 import style from './Search.css'
 import SearchInput from './../../component/SearchInput/SearchInput'
+import SearchList from './../../component/SearchList/SearchList'
 
 class Search extends Component {
+    constructor () {
+        super()
+        this.state = {
+            value: ''
+        }
+    }
+
+    /**
+     * 搜索框变化
+     * @param {String} value 搜索框内容
+     */
+    onInputChange = (value) => {
+        this.setState({
+            value
+        })
+    }
+
+    /**
+     * 清空搜索框
+     */
+    onInputClose = () => {
+        this.setState({
+            value: ''
+        })
+    }
+    
     render () {
+        const { value } = this.state
         return (
             <section className={style['search-wrapper']}>
-                <SearchInput/>
+                <SearchInput
+                    onInputClose={this.onInputClose}
+                    onInputChange={this.onInputChange}
+                    value={value}/>
+                <SearchList/>
             </section>
         )
     }
